86ef9fe1362e43c0257d1123059290b5831b49f4
E3fujita <fujita-ryusei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 15:38:53 +0900

    Update HomeSearchServlet.java

--- source/src/main/java/servlet/HomeSearchServlet.java
+++ source/src/main/java/servlet/HomeSearchServlet.java
@@ -1,9 +1,11 @@
 package servlet;
 
-import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -26,8 +28,9 @@ public class HomeSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ホーム＆検索画面にフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home_search.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -36,6 +39,11 @@ public class HomeSearchServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		// ホーム＆検索画面にフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home_search.jsp");
+		dispatcher.forward(request, response);
 	}
 
+	
 }
