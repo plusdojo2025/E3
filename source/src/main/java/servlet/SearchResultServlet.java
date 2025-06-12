d0dda75d9e0b951fb0b26fa8832e48c1c698034b
plusdojo2025-nishida <nishida-masamichi-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 15:32:43 +0900

    Update SearchResultServlet.java

--- source/src/main/java/servlet/SearchResultServlet.java
+++ source/src/main/java/servlet/SearchResultServlet.java
@@ -1,5 +1,58 @@
 package servlet;
 
-public class SearchResultServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SearchResultServlet
+ */
+@WebServlet("/SearchResultServlet")
+public class SearchResultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchResultServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// ログインしていない場合ログインサーブレットにリダイレクトする
+//		HttpSession session = request.getSession();
+//		if (session.getAttribute("id") == null) {
+//			response.sendRedirect("/E3/LoginServlet");
+//			return;
+//		}
+		
+		// 検索結果一覧ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/search_result.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// ログインしていない場合ログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/E3/LoginServlet");
+			return;
+		}
+		
+		
+	}
 
 }
