5c60d0c81a3f8906750efa1a338fb71c8f60215d
plusdojo2025-Baba <baba-junna-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 13:49:20 +0900

    Update IdPw.java

--- source/src/main/java/dto/IdPw.java
+++ source/src/main/java/dto/IdPw.java
@@ -1,5 +1,39 @@
 package dto;
 
 public class IdPw {
+	private int id; //ID
+	private String pass; // パスワード
+	private String email; // メールアドレス
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public IdPw(int id, String email, String pass) {
+		this.id = id;
+		this.email = email;
+		this.pass = pass;
+	}
+	public IdPw() {
+		this.id = 0;
+		this.email = "";
+		this.pass = "";
+	}
 }
