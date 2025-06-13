4de64767a664f1eed8c6c6fe8e795854d62a86d5
E3fujita <fujita-ryusei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 14:06:59 +0900

    Update Chat.java

--- source/src/main/java/dto/Chat.java
+++ source/src/main/java/dto/Chat.java
@@ -1,5 +1,43 @@
 package dto;
 
 public class Chat {
+    private String chatText;    // テキスト本文
+    private int roomId;         // ルームID
+    private int senderId;       // 送信者ID
+    private String chatDate;    // 送信日時
+	private int id;             // ID
+	
+	
+	public String getChatText() {
+		return chatText;
+	}
+	public void setChatText(String chatText) {
+		this.chatText = chatText;
+	}
+	public int getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+	public int getSenderId() {
+		return senderId;
+	}
+	public void setSenderId(int senderId) {
+		this.senderId = senderId;
+	}
+	public String getChatDate() {
+		return chatDate;
+	}
+	public void setChatDate(String chatDate) {
+		this.chatDate = chatDate;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+ 
 
 }
