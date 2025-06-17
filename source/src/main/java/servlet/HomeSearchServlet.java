826338a8490c357b7008e6fd0d26edbc9ca87d22
E3fujita <fujita-ryusei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 15:25:08 +0900

    Update HomeSearchServlet.java

--- source/src/main/java/servlet/HomeSearchServlet.java
+++ source/src/main/java/servlet/HomeSearchServlet.java
@@ -19,31 +19,35 @@ import dto.StandByUser;
 @WebServlet("/HomeSearchServlet")
 public class HomeSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeSearchServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public HomeSearchServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// ホーム＆検索画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home_search.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
+
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		int id = Integer.parseInt("id");
@@ -53,15 +57,18 @@ public class HomeSearchServlet extends HttpServlet {
 		String drop_off_longitude = request.getParameter("drop_off_longitude");
 		int headcount = Integer.parseInt(request.getParameter("headcount"));
 
-		// 登録処理を行う
-		StandByUserDao = new StandByUserDao();
-		List<StandByUser> userList = searchByCondition(date, current_latitude, current_longitude,
-				drop_off_latitude, drop_off_longitude, headcount);
+		// 検索処理を行う
+		StandByUserDao sbDao = new StandByUserDao();
+		List<StandByUser> userList = searchByCondition(date, current_latitude, current_longitude, drop_off_latitude,
+				drop_off_longitude, headcount);
 
-		
 		// 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
 		dispatcher.forward(request, response);
-	
+
+		
+		
+	}
 }
-}
+
+
