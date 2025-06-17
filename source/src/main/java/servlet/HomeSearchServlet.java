0f08cef10b296113db3908fe439eb932296b5873
E3fujita <fujita-ryusei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 13:49:37 +0900

    Update HomeSearchServlet.java

--- source/src/main/java/servlet/HomeSearchServlet.java
+++ source/src/main/java/servlet/HomeSearchServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.StandByUserDao;
+import dto.StandByUser;
 
 /**
  * Servlet implementation class HomeSearchServlet
@@ -40,10 +44,24 @@ public class HomeSearchServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 		
-		// ホーム＆検索画面にフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home_search.jsp");
-		dispatcher.forward(request, response);
-	}
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int id = Integer.parseInt("id");
+		String current_latitude = request.getParameter("current_latitude");
+		String current_longitude = request.getParameter("current_longitude");
+		String drop_off_latitude = request.getParameter("drop_off_latitude");
+		String drop_off_longitude = request.getParameter("drop_off_longitude");
+		int headcount = Integer.parseInt(request.getParameter("headcount"));
 
+		// 登録処理を行う
+		StandByUserDao = new StandByUserDao();
+		List<StandByUser> userList = searchByCondition(date, current_latitude, current_longitude,
+				drop_off_latitude, drop_off_longitude, headcount);
+
+		
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+		dispatcher.forward(request, response);
 	
+}
 }
