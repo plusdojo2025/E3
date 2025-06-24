ab94ebaffafd785c0f98499bc729b46236f5d78b
plusdojo2025-mizuno <mizuno-koume-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-24 09:38:34 +0900

    水野:未使用部分削除

--- source/src/main/java/servlet/AppointmentServlet.java
+++ source/src/main/java/servlet/AppointmentServlet.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dao.ChatDao;
-import dto.Chat;
 import dto.Taxi;;
 
 /**
@@ -57,48 +55,5 @@ public class AppointmentServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/appointment.jsp");
 		dispatcher.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	/*
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// ログイン状態チェック
-//		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect(request.getContextPath() + "/LoginServlet");
-//			return;
-//		}
-		
-		// テスト用　送信者id
-		int senderId = -11;
-		
-		// 申請情報id, 送信者id, チャットメッセージ, 送信日時を取得
-		request.setCharacterEncoding("UTF-8");
-		int roomId = Integer.parseInt(request.getParameter("room_id"));
-//		int senderId = (int) session.getAttribute("id");
-		String chatText = request.getParameter("selectPhrase");
-		String chatDate = request.getParameter("chat_date"); 
-		
-		// テスト用
-		System.out.println(roomId);
-		System.out.println(senderId);
-		System.out.println(chatText);
-		System.out.println(chatDate);
-		
-		// チャット登録処理
-		ChatDao chatDao = new ChatDao();
-		Chat chat = new Chat(0, roomId, senderId, chatText, chatDate);
-		// 実行結果判定
-		if (chatDao.insertChat(chat)) { 		// 登録が成功した場合
-			// テスト用
-			System.out.println("登録成功");
-		} else { 								// 登録失敗
-			// テスト用
-			System.out.println("登録失敗");
-		}
-		
-	}
-	*/
 
 }
